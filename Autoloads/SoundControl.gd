53bce74bbc6ea0f26b827f1b6a6639e2500320e8
PolyBytes <drtylero80@gmail.com>
2023-05-12 01:17:43 -0400

    Added basic version of the Pause Menu
    All buttons function and the pause_game input can be used to leave the settings menu and then leave the pause menu. I had to make a slight change to the Load.gd script because it was not setting the get_tree().current_scene to the new scene instance and it was returning null after the first scene transition. I also removed the deffered call to add_child and just called add_child instead because it was not playing nice with setting get_tree().current_scene.
    
    We need a PAUSED icon to replace the small label.
    
    Thanks =)

--- Autoloads/SoundControl.gd
+++ Autoloads/SoundControl.gd
@@ -5,7 +5,7 @@ onready var music_player = $MusicPlayer
 var track_1 = load("res://Sounds/Music/Track 1.mp3")
 
 func _ready():
-	pass
+	pause_mode = PAUSE_MODE_PROCESS
 
 func play_music():
 	music_player.stream = track_1
--- Menus/Pause/PauseMenu.gd
+++ Menus/Pause/PauseMenu.gd
@@ -1,20 +1,39 @@
-extends Control
+extends CanvasLayer
 
+var settings_menu: Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# To allow this node to still process and unpause the game while it is paused
+	pause_mode = PAUSE_MODE_PROCESS
 
+	visible = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	settings_menu = get_node_or_null("SettingsMenu")
+	assert(settings_menu != null, "[Error][PauseMenu] Attempted to get the SettingsMenu node but it was not found.")
+	settings_menu.visible = false
 
+func _process(_delta):
+	if Input.is_action_just_pressed("pause_game"):
+		if settings_menu.visible and get_tree().paused:
+			settings_menu.visible = false
+			return
+		else:
+			# This prevents a rare glitch that can occur if a player hits
+			# the settings but and the unpause button at the same time.
+			settings_menu.visible = false
+		
+		var paused: bool = not get_tree().paused
+		get_tree().paused = paused
+		visible = paused
 
-func _on_Resume_pressed():
+func _on_Button_Menu_pressed():
+	Load.change_scene(get_tree().current_scene, "res://Menus/MainMenu.tscn")
+	get_tree().paused = false
+	visible = false
+
+func _on_Button_Settings_pressed():
+	settings_menu.visible = true
+
+func _on_Button_Resume_pressed():
+	get_tree().paused = false
 	visible = false
