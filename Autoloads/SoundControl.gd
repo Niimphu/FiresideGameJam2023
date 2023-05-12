afb675f605fd4a4a79e5dbe4c5c5cbda3353d0cd
PolyBytes <drtylero80@gmail.com>
2023-05-12 01:24:38 -0400

    Fixed Music Bug
    There was a bug where if you went from the level back to the main menu the music would restart abruptly. It has been resolved. =)

--- Autoloads/SoundControl.gd
+++ Autoloads/SoundControl.gd
@@ -1,6 +1,6 @@
 extends Node
 
-onready var music_player = $MusicPlayer
+onready var music_player: AudioStreamPlayer = $MusicPlayer
 
 var track_1 = load("res://Sounds/Music/Track 1.mp3")
 
@@ -8,5 +8,8 @@ func _ready():
 	pause_mode = PAUSE_MODE_PROCESS
 
 func play_music():
+	if music_player.playing:
+		return
+
 	music_player.stream = track_1
 	music_player.play()
