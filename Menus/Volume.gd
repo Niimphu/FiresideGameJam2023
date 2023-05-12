7e77cdddf7861f8eeb81e56f820db3b7411e9791
Niimphu <nimphu@outlook.com>
2023-05-13 01:39:39 +0200

    volume slider values auto set themselves to bus values on ready()

--- Menus/Volume.gd
+++ Menus/Volume.gd
@@ -2,9 +2,16 @@ extends Control
 
 onready var music_bus = AudioServer.get_bus_index("Music")
 onready var sfx_bus = AudioServer.get_bus_index("SFX")
+onready var music_slider = $MarginContainer/VBoxContainer/HBoxContainer/Sliders/MusicSlider
+onready var sfx_slider = $MarginContainer/VBoxContainer/HBoxContainer/Sliders/SFXSlider
 
 func _ready():
+	var music_volume = AudioServer.get_bus_volume_db(music_bus)
+	var sfx_volume = AudioServer.get_bus_volume_db(sfx_bus)
+	
 	self.visible = false
+	music_slider.value = music_volume if music_volume >= -30 else -30
+	sfx_slider.value = sfx_volume if sfx_volume >= -30 else -30
 
 func change_volume(bus, value):
 	AudioServer.set_bus_volume_db(bus, value)
