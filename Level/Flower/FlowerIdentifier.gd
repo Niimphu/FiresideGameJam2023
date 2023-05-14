21ff88001672f5dbf02023761df3104e4325c176
Niimphu <nimphu@outlook.com>
2023-05-14 02:50:57 +0200

    petals shader and code

--- Level/Flower/FlowerIdentifier.gd
+++ Level/Flower/FlowerIdentifier.gd
@@ -3,20 +3,27 @@ extends Node2D
 onready var stamen = $Stamen
 onready var pollen = $Pollen
 onready var petals = $Petals
+onready var stem = $Stem
 
 enum evenness { ODD, EVEN }
 enum pollening { NO, YES }
-enum colour { WHITE, RED, MAGENTA, ROYAL_BLUE }
+enum colour { WHITE, RED, PURPOLLL, BLUE }
 enum roundness { ROUND, POINTY }
 enum amount { FEW, MANY }
+enum thorny { YES, NO }
+enum leafy { YES, NO }
 
 var stamen_evenness
 var has_pollen
 var petal_colour
 var petal_roundness
 var petal_amount
+var stem_thorny
+var stem_leafy
 
 func _ready():
+	randomize()
+	petals.material.set_shader_param("colour_index", randi() % 4)
 	initialise_features()
 
 func check_danger() -> bool:
@@ -50,40 +57,28 @@ func check_danger() -> bool:
 
 func initialise_features():
 	stamen_evenness = evenness.ODD if stamen.frame % 2  else evenness.EVEN
-
+	
 	has_pollen = pollening.NO if pollen.frame == 0 else pollening.YES
 	
 	petal_amount = amount.FEW if petals.frame < 2 else amount.MANY
 	
 	petal_roundness = roundness.POINTY if petals.frame == 1 or petals.frame == 2 else roundness.ROUND
-
+	
+	stem_thorny = thorny.YES if stem.frame > 1 else thorny.NO
+	
+	stem_leafy = leafy.YES if stem.frame % 2 else leafy.NO
+	
 	if pollen.frame == 0:
 		pollen.get_node("Particles2D").visible = false
 	else:
 		pollen.get_node("Particles2D").visible = true
-
-#	match petals.frame:
-#		0:
-#			petal_colour = colour.RED
-#			petal_roundness = roundness.ROUND
-#		1:
-#			petal_colour = colour.WHITE
-#			petal_roundness = roundness.ROUND
-#		2:
-#			petal_colour = colour.ORANGE
-#			petal_roundness = roundness.POINTY
-#		3:
-#			petal_colour = colour.BLUE
-#			petal_roundness = roundness.POINTY
-#		4:
-#			petal_colour = colour.WHITE
-#			petal_roundness = roundness.POINTY
-#		5:
-#			petal_colour = colour.ORANGE
-#			petal_roundness = roundness.POINTY
-#		6:
-#			petal_colour = colour.BLUE
-#			petal_roundness = roundness.ROUND
-#		7:
-#			petal_colour = colour.RED
-#			petal_roundness = roundness.ROUND
+	
+	match petal_colour:
+		0:
+			petal_colour = colour.WHITE
+		1:
+			petal_colour = colour.RED
+		2:
+			petal_colour = colour.PURPOLLL
+		3:
+			petal_colour = colour.BLUE
